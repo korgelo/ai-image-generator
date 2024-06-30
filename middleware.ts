52434d2edb907588b289df2dcaa2f63ed98dd7a6
korgelo <gelo.abalos@gmail.com>
2024-06-30 20:26:30 +0800

    initial commit

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const protectedRoutes = createRouteMatcher([
+  '/',
+  '/profile',
+  '/credits',
+  '/transformations/(.*)'
+])
+
+export default clerkMiddleware((auth, req) =>{
+  if(protectedRoutes(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
