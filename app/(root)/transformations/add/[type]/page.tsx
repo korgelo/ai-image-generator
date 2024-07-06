2ecb21581a50173bb21fd6a5fcaac76c70c01086
korgelo <gelo.abalos@gmail.com>
2024-07-06 15:55:02 +0800

    Modify all

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -5,8 +5,8 @@ import { getUserById } from '@/lib/actions/user.actions';
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
-const AddTransformationTypePage = async ({params:{type}}:SearchParamProps) => {
-  const {userId} = auth();
+const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const { userId } = auth();
   const transformation = transformationTypes[type];
 
   if(!userId) redirect('/sign-in')
@@ -15,19 +15,20 @@ const AddTransformationTypePage = async ({params:{type}}:SearchParamProps) => {
 
   return (
     <>
-    <Header 
-      title={transformation.title}
-      subtitle={transformation.subTitle}
-    />
-
-    <TransformationForm 
-      action="Add"
-      userId={user._id}
-      type={transformation.type as TransformationTypeKey}
-      creditBalance={user.creditBalance}
-    />
-    </>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
     
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
 
-
-const IBMPlex = IBM_Plex_Sans ({
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ['400','500','600','700'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
-  title: "Tinderafy",
-  description: "Ai-powered image generator",
+  title: "Imaginify",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -25,11 +25,11 @@ export default function RootLayout({
     <ClerkProvider appearance={{
       variables: { colorPrimary: '#624cf5' }
     }}>
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
